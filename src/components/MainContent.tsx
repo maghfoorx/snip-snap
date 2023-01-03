a01826d9bf582d9061bcbc207d3dd87f8e1dc749
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-03 14:29:25 +0000

    added an input and post button and interface

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
+interface PasteBinType {
+  id: number;
+  date: Date;
+  pasteBody: string;
+}
 export default function MainContent(): JSX.Element {
-    return (
-        <>
-        <h1>HELLO</h1>
-        </>
-    )
+  const [pasteBinInput, setPasteBinInput] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <h1>HELLO</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={pasteBinInput}
+          onChange={(e) => setPasteBinInput(e.target.value)}
+        />
+        <button type="submit">Post</button>
+      </form>
+    </>
+  );
 }
